8147f79def266d89e0a24abf512cad96a430e742
sanggi-wjg <girr311@naver.com>
2022-06-08 17:55:10 +0900

    2022 06 08

--- src/main/java/cheat/sheet/sample/lambda/LambdaSampleMain.java
+++ src/main/java/cheat/sheet/sample/lambda/LambdaSampleMain.java
@@ -2,6 +2,8 @@ package cheat.sheet.sample.lambda;
 
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdaSampleMain {
@@ -52,8 +54,8 @@ public class LambdaSampleMain {
 
     public static void funcInterface() {
         /*
-        * https://devkingdom.tistory.com/292?category=941391
-        * */
+         * https://devkingdom.tistory.com/292?category=941391
+         * */
 
         /*
          * Supplier
@@ -61,20 +63,58 @@ public class LambdaSampleMain {
          *
          * T get() 추상 메소드가 있다
          * */
-        Supplier<String> supplier = () -> "Hi Hello";
+        Supplier<String> supplier = () -> "This is Supplier";
         System.out.println(supplier.get());
+        System.out.println("====================================================================");
 
         /*
-        * Consumer
-        * 객체 T를 매개변수로 받아서 사용하고 No Return 함수형 인터페이스
-        *
-        * void accept(T t) 추상 메소드가 있다. 이 함수를 이용하면 하나의 함수가 끝났을때 다음 Consumer 를 계속해서 사용할수 있다.
-        * */
-        Consumer<String> consumer1 = s -> System.out.println("Hi " + s);
-        Consumer<String> consumer2 = s -> System.out.println("World");
-        consumer1.andThen(consumer2).accept("Snow");
-    }
+         * Consumer
+         * 객체 T를 매개변수로 받아서 사용하고 No Return 함수형 인터페이스
+         *
+         * void accept(T t) : 하나의 함수가 끝났을때 다음 Consumer 를 계속해서 사용할수 있다.
+         * */
+        Consumer<String> consumer1 = s -> System.out.print("[" + s);
+        Consumer<String> consumer2 = s -> System.out.println("]");
+        consumer1.andThen(consumer2).accept("Consumer");
+        System.out.println("====================================================================");
 
+        /*
+         * Function
+         * 객체 T를 매개변수로 받아서 로직을 처리하고 R을 Return 하는 함수형 인터페이스
+         *
+         * R apply(T t)
+         *
+         * andThen 관련
+         * https://seeminglyjs.tistory.com/250
+         * */
+        Function<String, Boolean> isContainFunction = s -> s.contains("a");
+        Function<String, Boolean> isEqualFunction = s -> s.equals("abc");
+
+        Boolean isContainFunctionResult = isContainFunction.apply("abc");
+        System.out.println("Function: " + isContainFunctionResult);
+
+        Function<String, Function<String, Boolean>> function = isContainFunction.andThen(t -> isEqualFunction);
+        Boolean functionResult = function.apply("abc").apply("a");
+        System.out.println("functionResult = " + functionResult);
+        System.out.println("====================================================================");
+
+        /*
+         *
+         * */
+        String testString = "abc";
+
+        Predicate<String> predicateContain = s -> s.contains("a");
+        Predicate<String> predicateEqual = s -> s.equals("abc");
+
+        boolean predicateResult = predicateContain.test(testString);
+        System.out.println("Predicate: " + predicateResult);
+
+        boolean predicateAndResult = predicateContain.and(predicateEqual).test(testString);
+        System.out.println("Predicate And: " + predicateAndResult);
+
+        boolean predicateOrResult = predicateContain.or(predicateEqual).test(testString);
+        System.out.println("Predicate Or: " + predicateOrResult);
+    }
 
 
     public static void main(String[] args) {
